1c5b240cf45f686f4e5e5161eabd6674d9533787
jobsicke7 <doh292929@gmail.com>
2024-12-11 08:07:01 +0900

    go home

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <h1 style={style.title}>Error 404</h1>
       <h2 style={style.sub}>링크가 이전되었거나, 삭제되었을 수 있습니다.</h2>
       <button style={style.button} onClick={navigateToHome}>
-        Go to Home1
+        Go to Home
       </button>
     </div>
   );
