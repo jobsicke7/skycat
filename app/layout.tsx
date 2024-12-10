22a5e1b2dd3ae3b6c3d7e84fa027228f6614e83b
jobsicke7 <doh292929@gmail.com>
2024-12-11 00:45:28 +0900

    add naver login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import localFont from "next/font/local";
 import "./globals.css";
 import GNB from "../components/GNB";
 import FNB from "../components/FNB";
@@ -8,17 +7,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import { SessionProvider } from "next-auth/react";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function RootLayout({
   children,
